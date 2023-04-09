c4a707deb092eaabcfbb0f74daf7a35e32fbe34c
Faeez Shaikh <abdullah.fshkh@gmail.com>
2023-04-09 13:12:20 -0500

    updates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from langchain import PromptTemplate
 from langchain.llms import OpenAI
 
 template = """
-    Below is a snippet of code that may be poorly worded.
+    Below is a snippet of code.
     Your goal is to:
     - Understand the code, and explain it in plain english.
     - Print the purpose of the code.
@@ -15,7 +15,11 @@ template = """
     Print the purpose with a title like this: 
     \nPURPOSE:
     \nEXPLANATION:
+    \nISSUES/BUGS: 
+    \nOPTIMIZATION:
+    \nINEFFICIENCIES:
 
+    When the action is 'Generate Unit test', you can omit everything else (ie. do not give any explanation or any other text besides the unit test code) and you should just write a correct unit test in the language specified.
     Below is the language, and action to be performed on the code:
     LANGUAGE: {language}
     ACTION: {action}
@@ -25,11 +29,29 @@ template = """
     YOUR RESPONSE:
 """
 
+
+template_unittest = """
+    Below is a snippet of code.
+    Your goal is to:
+    - Understand the code and generate a unit test in the language specified.
+    You should just write a correct unit test in the language specified.
+    Below is the language, and action to be performed on the code:
+    LANGUAGE: {language}
+    ACTION: {action}
+    CODE: {code}
+
+    
+    YOUR RESPONSE:
+"""
 prompt = PromptTemplate(
     input_variables=["language", "action","code"],
     template=template,
 )
 
+prompt_for_unit_test = PromptTemplate(
+    input_variables=["language", "action","code"],
+    template=template_unittest,
+)
 def load_LLM(openai_api_key):
     """Logic for loading the chain you want to use should go here."""
     # Make sure your openai_api_key is set as an environment variable
@@ -42,7 +64,7 @@ st.header("Review Code")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("A simple code review tool.")
+    st.markdown("A simple code review tool by Faeez Shaikh")
 
 # with col2:
     # st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
@@ -59,12 +81,12 @@ col1, col2 = st.columns(2)
 with col1:
     programming_language = st.selectbox(
         'Select the programming language for this snippet:',
-        ('Java', 'Javascript','Python'))
+        ('Java','Typescript with JSX syntax', 'Javascript','Python',))
     
 with col2:
     requested_action = st.selectbox(
         'Select what action you would like to do:',
-        ('Optimize','Explain','Find bugs'))
+        ('Optimize','Explain','Find bugs','Generate Unit test'))
 
 def get_text():
     input_text = st.text_area(label="Code input", label_visibility='collapsed', placeholder="Your Code...", key="code_input")
@@ -82,9 +104,10 @@ def update_text_with_example():
  return a-b;
 }"""
 
-st.button("*See An Example*", type='secondary', help="Click to see an example of how code is reviewed.", on_click=update_text_with_example)
-
 st.markdown("### Your Code Explained:")
+
+# def perform_submit_action():
+    # print ("in submit")
 
 if code_input:
     if not openai_api_key:
@@ -93,8 +116,25 @@ if code_input:
 
     llm = load_LLM(openai_api_key=openai_api_key)
 
-    prompt_with_email = prompt.format(language=programming_language, action=requested_action, code=code_input)
+    st.write({programming_language},{requested_action})
 
-    formatted_email = llm(prompt_with_email)
+    if requested_action == 'Generate Unit test':
+        prompt_for_unit_test = prompt_for_unit_test.format(language=programming_language, action=requested_action, code=code_input)
+        ai_response = llm(prompt_for_unit_test)
+        st.code(ai_response, language='{programming_language}', line_numbers=True)
+    else:
+        prompt_for_code_comments = prompt.format(language=programming_language, action=requested_action, code=code_input)
+        ai_response = llm(prompt_for_code_comments)
+        st.write(ai_response)
 
-    st.write(formatted_email)
+st.button("*See An Example*", type='secondary', help="Click to see an example of how code is reviewed.", on_click=update_text_with_example)
+
+
+# col1, col2 = st.columns(2)
+# with col1:
+    
+# with col2:
+    # st.button("Submit", type="primary", disabled=False, use_container_width=True,on_click=perform_submit_action)
+
+
+
